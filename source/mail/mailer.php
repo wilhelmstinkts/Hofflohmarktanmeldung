087674ebb8d9f36e2aa0478bb7666ae2efafa1ea
Philipp Just <philipp.just@hrmony.de>
2023-05-03 21:37:40 +0200

    Fix E-Mail Header

--- source/mail/mailer.php
+++ source/mail/mailer.php
@@ -1,21 +1,27 @@
 <?php
+
 namespace Mail;
 
-class Mailer {
-
-    private static string $headers = 
-        'From: Wilhelm Gibt Keine Ruh <no-reply@wilhelm-gibt-keine-ruh.de>\r\n'
-        .'Reply-To: <no-reply@wilhelm-gibt-keine-ruh.de>\r\n'
-        .'Content-Type: text/html\r\n';
-    
+class Mailer
+{
+    private static function getHeaders()
+    {
+        return array(
+            'From' => 'Wilhelm Gibt Keine Ruh <no-reply@wilhelm-gibt-keine-ruh.de>',
+            'Reply-To' => 'no-reply@wilhelm-gibt-keine-ruh.de',
+            'Content-Type' => 'text/html',
+            'X-Mailer' => 'PHP/' . phpversion()
+        );
+    }
 
     public static function sendeAnmeldeBestaetigung(string $empfaengerEmail, string $absageCode)
     {
+
         $message = <<<EOD
             <p>Vielen Dank für Ihre Anmeldung zum Hofflohmarkt.</p>
             <p>Falls sich Ihre Pläne ändern, können Sie <a href="https://hofflohmarkt.wilhelm-gibt-keine-ruh.de/abmeldung.php?code={$absageCode}">hier</a> Ihre Teilnahme absagen.</p>
         EOD;
 
-        mail($empfaengerEmail, 'Anmeldung Hofflohmarkt', $message, Mailer::$headers);
+        mail($empfaengerEmail, 'Anmeldung Hofflohmarkt', $message, Mailer::getHeaders());
     }
 }
